7b6412297fa632377b138cbf36d62a54296d25f9
Freinholm <aaron.frein@presidio.edu>
2026-03-02 09:12:33 -0800

    Implement initial heading & paragraph extraction methods & tests.

--- crawl.py
+++ crawl.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 
@@ -6,3 +7,21 @@ def normalize_url(url):
     full_path = f"{parsed_url.netloc}{parsed_url.path}"
     full_path = full_path.rstrip("/")
     return full_path.lower()
+
+
+def get_heading_from_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    h_tag = soup.find("h1") or soup.find("h2")
+    return h_tag.get_text(strip=True) if h_tag else ""
+
+
+def get_first_paragraph_from_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+
+    main_section = soup.find("main")
+    if main_section:
+        first_p = main_section.find("p")
+    else:
+        first_p = soup.find("p")
+
+    return first_p.get_text(strip=True) if first_p else ""
