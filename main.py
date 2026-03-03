aa8ff091d201f405aa8e61362f4a201cd7d1b61c
freinholm <aaron.frein@presidio.edu>
2026-03-03 09:41:19 -0800

    Write out page data in a JSON report.

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import sys
 import asyncio
-from crawl import crawl_page_async
+from crawl import crawl_site_async
+from json_report import write_json_report
 
 async def main():
     args = sys.argv
-    if len(args) < 2:
-        print("no website provided")
+    if len(args) < 4:
+        print("usage python main.py <base_url> <max_concurrency> <max_pages>")
         sys.exit(1)
-    if len(args) > 2:
+    if len(args) > 4:
         print("too many arguments provided")
         sys.exit(1)
 
     base_url = args[1]
 
-    print(f"starting crawl of: {base_url}...")
+    if not args[2].isdigit():
+        print("max_concurrency must be an integer")
+        sys.exit(1)
+    if not args[3].isdigit():
+        print("max_pages must be an integer")
+        sys.exit(1)
 
-    page_data = await crawl_page_async(base_url)
+    max_concurrency = int(args[2])
+    max_pages = int(args[3])
 
-    print(f"Found {len(page_data)} pages:")
-    for page in page_data.values():
-        print(f"Found {len(page['outgoing_links'])} outgoing links on {page['url']}")
+    print(f"Starting async crawl of: {base_url}...")
+
+    page_data = await crawl_site_async(base_url, max_concurrency, max_pages)
+
+    print(f"Crawling complete. Found {len(page_data)} pages.")
+
+    write_json_report(page_data)
 
     sys.exit(0)
 
